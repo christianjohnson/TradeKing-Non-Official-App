d6859320cad803555cc624bb1f9697fabd0a3e41
Christian Johnson <christian@chrjo.com>
2011-05-11 00:57:08 -0400

    added sample keys

---
+++ src/com/chrjo/tradeking/keys.java
@@ -0,0 +1,9 @@
+package com.chrjo.tradeking;
+
+public class keys {
+	 public static String appKey = "XXXXX"; 
+	 public static String userKey = "XXXX"; 
+	 public static String userSecret = "XXX";
+	 public static String accountNumber = "XXXXXXX";
+}
+
