3ec30e25fb057ffd7c27a99def70c141aff7d807
Christian Johnson <christian@chrjo.com>
2011-05-11 00:46:02 -0400

    Added basic functionality

--- src/com/chrjo/tradeking/MainActivity.java
+++ src/com/chrjo/tradeking/MainActivity.java
@@ -1,13 +1,66 @@
 package com.chrjo.tradeking;
 
+import java.text.DecimalFormat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private TradeKingAPI tk = new TradeKingAPI();
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				queryTK();
+			}
+		});
+        queryTK();
+    }
+    
+    public void queryTK(){
+    	String response = tk.run();
+        TextView t = (TextView) findViewById(R.id.textView1);
+        DecimalFormat twoDForm = new DecimalFormat("#.##");
+		try {
+			JSONObject object = (JSONObject) new JSONObject(response);
+	        JSONObject locations = object.getJSONObject("response");
+	        JSONObject accountholdings = locations.getJSONObject("accountholdings");
+	        JSONArray holdings = accountholdings.getJSONArray("holding");
+	        String chart = "";
+	        double amount = 0.0;
+	        for (int i=0;i < holdings.length();i++){
+	        	JSONObject obj = holdings.getJSONObject(i);
+	        	Double gain = Double.parseDouble(obj.getString("gainloss"));
+	        	Double pos = Math.abs(Double.parseDouble(obj.getString("qty")));
+	        	Double cost = Double.parseDouble(obj.getString("costbasis"));
+	        	Double value = Double.parseDouble(obj.getString("marketvalue"));
+	        	value = Double.valueOf(twoDForm.format(value));
+	        	amount += gain;
+	        	chart += (obj.getJSONObject("instrument").getString("desc") + "\n\t Cost: $" + cost + ", Value: $" + value + "\n");
+	        }
+	        
+	        //Round
+	        amount = Double.valueOf(twoDForm.format(amount));
+			
+	        t.setText("Total Gain/Loss is $" + amount + "\n" + chart);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
--- src/com/chrjo/tradeking/TradeKingAPI.java
+++ src/com/chrjo/tradeking/TradeKingAPI.java
@@ -1,26 +1,31 @@
 package com.chrjo.tradeking;
 
-import java.security.SignatureException; 
-import java.util.Calendar; 
-import java.net.URL; 
-import java.net.URLConnection; 
-import java.io.DataOutputStream; 
-import java.io.BufferedReader; 
-import java.io.InputStreamReader; 
-import javax.crypto.Mac; 
-import javax.crypto.spec.SecretKeySpec; 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.SignatureException;
+import java.util.Calendar;
 
-import org.apache.commons.codec.binary.Hex; 
-import org.apache.commons.codec.binary.Base64; 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 public class TradeKingAPI { 
+	
   public static String generateSignature(String data, String key) throws java.security.SignatureException { 
-    String result; 
-    try { 
+    String result;
+    try {
       byte[] encodedData = Base64.encodeBase64(data.getBytes()); 
       // Get an hmac_md5 key from the raw key bytes
       byte[] keyBytes = key.getBytes(); 
-      SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacMD5"); 
+      SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacMD5");
       // Get an hmac_md5 Mac instance and initialize with the signing key 
       Mac mac = Mac.getInstance("HmacMD5"); 
       mac.init(signingKey); 
@@ -30,7 +35,7 @@ public class TradeKingAPI {
       byte[] hexBytes = new Hex().encode(rawHmac); 
       //  Covert array of Hex bytes to a String 
       result = new String(hexBytes, "ISO-8859-1"); 
-    } 
+    }
     catch (Exception e) { 
       throw new SignatureException("Failed to generate HMAC : " + e.getMessage()); 
     } 
@@ -44,49 +49,47 @@ public class TradeKingAPI {
 	      String timestamp    = String.valueOf(Calendar.getInstance().getTimeInMillis()); 
 	      String request_data = body + timestamp; 
 	      String signature = TradeKingAPI.generateSignature(request_data, userSecret); 
-	      URL url = new URL(resourceUrl); 
-	      URLConnection conn = url.openConnection(); 
+	      URL url = new URL(resourceUrl);
+	      HttpsURLConnection conn = (HttpsURLConnection) url.openConnection(); 
 	      conn.setDoInput (true); 
 	      conn.setDoOutput (true); 
 	      conn.setUseCaches (false); 
 	      conn.setRequestProperty("Content-Type", "application/xml"); 
-	      conn.setRequestProperty("Accept", "application/xml"); 
+	      conn.setRequestProperty("Accept", "application/json"); 
 	      conn.setRequestProperty("TKI_TIMESTAMP", timestamp); 
 	      conn.setRequestProperty("TKI_SIGNATURE", signature); 
 	      conn.setRequestProperty("TKI_USERKEY", userKey); 
 	      conn.setRequestProperty("TKI_APPKEY", appKey); 
-	      DataOutputStream out = new DataOutputStream (conn.getOutputStream ()); 
+	      DataOutputStream out = new DataOutputStream (conn.getOutputStream()); 
 	      out.writeBytes(body); 
 	      out.flush(); 
-	      out.close(); 
-	      BufferedReader in = new BufferedReader (new InputStreamReader(conn.getInputStream ())); 
+	      out.close();
+	      BufferedReader in = new BufferedReader (new 
+	      InputStreamReader(conn.getInputStream ())); 
 	      String temp; 
 	      while ((temp = in.readLine()) != null){ 
 	        response += temp + "\n"; 
 	      } 
 	      temp = null; 
 	      in.close(); 
+	      conn.disconnect();
 	      return response; 
 	    } 
 	    catch(java.security.SignatureException e) { 
-	      System.out.println("Error " + e); 
-	      return ""; 
+	      return "Signature Error: " + e; 
 	    } 
 	    catch(java.io.IOException e) { 
-	      System.out.println("Error " + e); 
-	      return ""; 
+	      return "IO Error: " + e; 
 	    } 
-	  } 
+	  }
   
-	  public static void main(String[] args) { 
-	    String appKey    = "679d0e393f523e6d7b435aba3b2ebe33"; 
-	    String userKey    = "476a90129381a6454a3f265f753f624d"; 
-	    String userSecret   = "25a3fb239b20f94f76efa254936f626e"; 
-	    
-	    String body     = 
-	    	"<request><account>12345678</account><quote><watchlist>DEFAULT</watchlist><delayed>true</delayed></quote></request>"; 
-	    	    String response = TradeKingAPI.request("https://<url>/beta/trade/quotes", body, 
-	    	appKey, userKey, userSecret); 
-	    	    System.out.println("Server response:\n'" + response + "'"); 
-	  } 
+  	  public String run(){
+  		String appKey = keys.appKey;
+	    String userKey = keys.userKey;
+	    String userSecret = keys.userSecret;
+	    String accountNumber = keys.accountNumber;
+	    String body = 
+	    	"<request><account>" + accountNumber + "</account></request>";
+  		return TradeKingAPI.request("https://tkapi.tradeking.com/beta/account/holdings", body, appKey, userKey, userSecret);
+  	  }
 }
